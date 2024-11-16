679df2dca367143aea8df5eb2434a25284f66dfa
ClaytonCKOB <claytoncaco89@gmail.com>
2024-11-15 21:23:18 -0300

    feat: adding title and changing api service in docker-compose

--- service/index.js
+++ service/index.js
@@ -2,7 +2,14 @@ const {CampaignModel} = require('./Models/CampaignModel')
 const { UserModel } = require('./Models/UserModel')
 const app = require('express')()
 const PORT = 8080
+const cors = require('cors');
+const corsOptions = {
+    origin: 'http://localhost:3000', // Replace with the frontend's URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
+app.use(cors(corsOptions));
 
 app.listen(PORT, ()=>{
     console.log(`Server is running at http://localhost:${PORT} `)
@@ -17,4 +24,3 @@ app.get("/users", async (req, res) => {
     const usersData = await UserModel.getAll()
     res.status(200).send(usersData)
 })
-
